4f94e752fa24ec3aeba8ecbd08fe8f5db0425b7b
dev-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-12-26 20:13:26 +0600

    custom hook used

--- src/pages/OurMenu/OurMenu.jsx
+++ src/pages/OurMenu/OurMenu.jsx
@@ -1,21 +1,15 @@
 import { Helmet } from "react-helmet";
 import PageBanner from "../../components/PageBanner/PageBanner";
-import { useEffect, useState } from "react";
 import MenuCard from "../../components/Cards/MenuCard/MenuCard";
 import ourMenuImg from "../../assets/images/our-menu/our-menu-banner.jpg";
 import saladImg from "../../assets/images/our-menu/salad-bg.jpg";
 import soupImg from "../../assets/images/our-menu/soup-bg.jpg";
 import dessertImg from "../../assets/images/our-menu/dessert-bg.jpeg";
 import drinksImg from "../../assets/images/our-menu/drink-bg.jpg";
+import { useMenu } from "../../hooks/useMenu";
 
 const OurMenu = () => {
-  const [menu, setMenu] = useState([]);
-
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => setMenu(data));
-  }, []);
+  const [menu] = useMenu();
 
   const offeredCollection = menu.filter((item) => item.category === "offered");
   const saladCollection = menu.filter((item) => item.category === "salad");
@@ -30,7 +24,7 @@ const OurMenu = () => {
       </Helmet>
 
       <PageBanner img={ourMenuImg} title="Our Menu" />
-      <h1 className="menu-heading">Today's Offer</h1>
+      <h1 className="menu-heading">Todays Offer</h1>
       <div className="menu-grid">
         {offeredCollection.map((item) => (
           <MenuCard key={item._id} item={item} />
@@ -44,11 +38,19 @@ const OurMenu = () => {
         ))}
       </div>
 
+      <div className="menu-button-box">
+        <button className="menu-button">Order your favorite food</button>
+      </div>
+
       <PageBanner img={soupImg} title="Soup" />
       <div className="menu-grid">
         {soupCollection.map((item) => (
           <MenuCard key={item._id} item={item} />
         ))}
+      </div>
+
+      <div className="menu-button-box">
+        <button className="menu-button">Order your favorite food</button>
       </div>
 
       <PageBanner img={dessertImg} title="Dessert" />
@@ -58,11 +60,19 @@ const OurMenu = () => {
         ))}
       </div>
 
+      <div className="menu-button-box">
+        <button className="menu-button">Order your favorite food</button>
+      </div>
+
       <PageBanner img={drinksImg} title="Cocktail" />
       <div className="menu-grid">
         {drinksCollection.map((item) => (
           <MenuCard key={item._id} item={item} />
         ))}
+      </div>
+
+      <div className="menu-button-box">
+        <button className="menu-button">Order your favorite food</button>
       </div>
     </div>
   );
